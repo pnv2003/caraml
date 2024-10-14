e207a61fd06712987d4327e03725c90090569c84
Phuong Ngo <phuongngovan2003@gmail.com>
2024-10-14 13:04:19 +0700

    Add label support

--- models/base.py
+++ models/base.py
@@ -1,7 +1,23 @@
 class Model:
 
+    def __init__(self):
+        self.xlabels = None
+        self.ylabel = None
+
     def fit(self, X, y):
         raise NotImplementedError()
     
     def predict(self, X):
         raise NotImplementedError()
+    
+    def get_xlabel(self, index):
+
+        if self.xlabels is None:
+            return "Feature " + str(index)
+        return self.xlabels[index]
+    
+    def get_ylabel(self):
+
+        if self.ylabel is None:
+            return "Target"
+        return self.y_label
