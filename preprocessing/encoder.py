e07865b097e554b21adb11442f5ccb1e49b38cc0
Phuong Ngo <phuongngovan2003@gmail.com>
2024-10-14 12:38:42 +0700

    Fix onehot encoder: support 2D

---
+++ preprocessing/encoder.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+class LabelEncoder:
+
+    def __init__(self) -> None:
+        
+        self.map = None
+        self.invmap = None
+    
+    def fit(self, y):    
+        self.map = {}
+        self.invmap = {}
+        for i, k in enumerate(set(y)):
+            self.map[k] = i
+            self.invmap[i] = k
+
+    def transform(self, y):
+        return np.array([self.map[k] for k in y])
+    
+    def fit_transform(self, y):
+        self.fit(y)
+        return self.transform(y)
+
+    def inverse_transform(self, y):
+        return np.array([self.invmap[i] for i in y])
+
+class OneHotEncoder:
+    
+    def __init__(self) -> None:
+
+        self.maps = None
+        self.invmaps = None
+    
+    def fit(self, X):
+
+        self.maps = []
+        self.invmaps = []
+
+        for i in range(X.shape[1]):
+            map = {}
+            invmap = {}
+
+            dim = len(set(X[:, i]))
+            for j, k in enumerate(set(X[:, i])):
+                vec = np.zeros(dim)
+                vec[j] = 1
+                map[k] = vec
+                invmap[tuple(vec)] = k
+
+            self.maps.append(map)
+            self.invmaps.append(invmap)
+            
+    def transform(self, X):
+        
+        res = []
+        for i in range(X.shape[1]):
+            res.append(np.array([self.maps[i][k] for k in X[:, i]]))
+        return np.concatenate(res, axis=1)
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
+
+    def inverse_transform(self, X):
+
+        res = []
+        dims = [len(map) for map in self.maps]
+
+        l, r = None, X
+        for i, d in enumerate(dims):
+            l, r = r[:, :d], r[:, d:]
+            res.append(np.array([
+                self.invmaps[i][tuple(v)]
+                for v in l
+            ]))
+            
+        return np.column_stack(res)
+    
+    def get_encoded_labels(self, label):
+        return [
+            label + "_" + self.invmap[tuple(v)]
+            for v in self.map.values()
+        ]
